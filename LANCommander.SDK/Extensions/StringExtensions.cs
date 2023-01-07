c571058817bfe9e516d513fbe1dde07413451ade
Pat Hartl <pat@pathar.tl>
2023-01-07 12:57:58 -0600

    Replace colons in titles with hyphens for file safety

--- LANCommander.SDK/Extensions/StringExtensions.cs
+++ LANCommander.SDK/Extensions/StringExtensions.cs
@@ -7,9 +7,13 @@ namespace LANCommander.SDK.Extensions
     {
         public static string SanitizeFilename(this string filename, string replacement = "")
         {
+            var colonInTitle = new Regex(@"(\w)(: )(\w)");
             var removeInvalidChars = new Regex($"[{Regex.Escape(new string(Path.GetInvalidFileNameChars()))}]", RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.CultureInvariant);
 
-            return removeInvalidChars.Replace(filename, replacement);
+            filename = colonInTitle.Replace(filename, "$1 - $3");
+            filename = removeInvalidChars.Replace(filename, replacement);
+
+            return filename;
         }
     }
 }

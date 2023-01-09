1c51b2643e9484578b4f307ca131f40142c2a8d5
Pat Hartl <pat@pathar.tl>
2023-01-09 01:35:47 -0600

    Cascade delete archives when game is deleted

---
+++ LANCommander/Migrations/20230109073520_CascadeGameArchiveDeletion.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LANCommander.Migrations
+{
+    public partial class CascadeGameArchiveDeletion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Archive_Games_GameId",
+                table: "Archive");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Archive_Games_GameId",
+                table: "Archive",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Archive_Games_GameId",
+                table: "Archive");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Archive_Games_GameId",
+                table: "Archive",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id");
+        }
+    }
+}

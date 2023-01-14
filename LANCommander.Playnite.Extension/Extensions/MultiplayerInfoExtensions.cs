ae6cd7f90632d1f3cf12bffefc429bc11745eb8c
Pat Hartl <pat@pathar.tl>
2023-01-14 15:11:16 -0600

    Added extension for grabbing multiplayer player count for features

---
+++ LANCommander.Playnite.Extension/Extensions/MultiplayerInfoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using LANCommander.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LANCommander.PlaynitePlugin.Extensions
+{
+    internal static class MultiplayerInfoExtensions
+    {
+        internal static string GetPlayerCount(this MultiplayerInfo multiplayerInfo)
+        {
+            if (multiplayerInfo.MinPlayers == multiplayerInfo.MaxPlayers && multiplayerInfo.MinPlayers >= 2)
+                return $"({multiplayerInfo.MinPlayers} Players)";
+
+            if (multiplayerInfo.MinPlayers < multiplayerInfo.MaxPlayers && multiplayerInfo.MinPlayers >= 2)
+                return $"({multiplayerInfo.MinPlayers}-{multiplayerInfo.MaxPlayers} Players)";
+
+            if (multiplayerInfo.MinPlayers <= 1 && multiplayerInfo.MaxPlayers > 2)
+                return $"({multiplayerInfo.MaxPlayers} Players)";
+
+            return "";
+        }
+    }
+}

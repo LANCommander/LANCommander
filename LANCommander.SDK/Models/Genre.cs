97c32895da5814b4e70433d39f23291b9d0e2cb2
Pat Hartl <pat@pathar.tl>
2023-01-14 15:10:53 -0600

    Added Genre to SDK models

---
+++ LANCommander.SDK/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace LANCommander.SDK.Models
+{
+    public class Genre : BaseModel
+    {
+        public string Name { get; set; }
+        public virtual IEnumerable<Game> Games { get; set; }
+    }
+}

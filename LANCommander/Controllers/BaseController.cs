f2dfc90de8aec4dc36ea8aefa06daa98d9861074
Pat Hartl <pat@pathar.tl>
2023-01-16 02:38:13 -0600

    Added ability to add alerts onto the page via controller

---
+++ LANCommander/Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace LANCommander.Controllers
+{
+    public class BaseController : Controller
+    {
+        public void Alert(string message, string type = "info", bool dismissable = true)
+        {
+            List<AlertViewModel> alerts;
+
+            try
+            {
+                alerts = JsonSerializer.Deserialize<List<AlertViewModel>>((string)TempData["Alerts"]);
+            }
+            catch
+            {
+                alerts = new List<AlertViewModel>();
+            }
+
+            alerts.Add(new AlertViewModel()
+            {
+                Message = message,
+                Type = type,
+                Dismissable = dismissable
+            });
+
+            TempData["Alerts"] = JsonSerializer.Serialize(alerts);
+        }
+    }
+}
---
+++ LANCommander/Models/AlertViewModel.cs
@@ -0,0 +1,6 @@
+﻿public class AlertViewModel
+{
+    public string Message { get; set; }
+    public bool Dismissable { get; set; }
+    public string Type { get; set; }
+}

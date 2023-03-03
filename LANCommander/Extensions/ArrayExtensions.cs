d24063b5453e8699b0078b8b45a1d98a0ede5180
Pat Hartl <pat@pathar.tl>
2023-03-02 22:56:11 -0600

    Started porting dashboard stats to AntDesign.
    Components can't be rendered with server prerendered??

---
+++ LANCommander/Extensions/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace LANCommander.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[] ShiftArrayAndInsert<T>(this T[] array, T input, int max)
+        {
+            if (array == null || array.Length < max)
+            {
+                array = new T[max];
+            }
+
+            Array.Copy(array, 1, array, 0, array.Length - 1);
+
+            array[array.Length - 1] = input;
+
+            return array;
+        }
+    }
+}

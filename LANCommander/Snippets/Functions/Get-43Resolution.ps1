42db70e3f59e6abb8549736d16fbef506ae96753
Pat Hartl <pat@pathar.tl>
2023-03-15 17:45:50 -0500

    New snipper for calculating the 4:3 resolution of the client's display

---
+++ LANCommander/Snippets/Functions/Get-43Resolution.ps1
@@ -0,0 +1,14 @@
+﻿function Get-43Resolution([int]$Width, [int]$Height) {
+    $ratio = 4 / 3
+
+    if (($Width -gt $Height) -or ($Width -eq $Height)) {
+        return @{ Width = [math]::Round($ratio * $Height); Height = $Height }
+    }
+
+    if ($Width -lt $Height) {
+        return @{ Width = $Width; Height = [math]::Round($Width / $ratio) }
+    }
+}
+
+# Accessible via $Resolution.Height, $Resolution.Width
+$Resolution = Get-43Resolution -Width 1280 -Height 800

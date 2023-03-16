460f40c2cde4b790326c46ddee6a217e9ea66304
Pat Hartl <pat@pathar.tl>
2023-03-15 23:18:12 -0500

    Fix virtual store snippet to use correct registry pathing

--- LANCommander/Snippets/Examples/Get User Registry Virtual Store.ps1
+++ LANCommander/Snippets/Examples/Get User Registry Virtual Store.ps1
@@ -1,4 +1,4 @@
 ﻿$User = New-Object System.Security.Principal.NTAccount($env:UserName)
 $SID = $User.Translate([System.Security.Principal.SecurityIdentifier]).value
 # The OS might force your game to read/write to the user's virtual store. This will write a key to the correct SID's virtual store.
-New-ItemProperty -Path "HKU:\$SID\Software\Classes\VirtualStore\MACHINE\SOFTWARE\WOW6432Node\<Path>" -Name "KeyName" -Value "New Value" -Force
+New-ItemProperty -Path "registry::\HKEY_USERS\$SID\Software\Classes\VirtualStore\MACHINE\SOFTWARE\WOW6432Node\<Path>" -Name "KeyName" -Value "New Value" -Force

c2dd624cec9ea043ea74fcaecad17e19080ac594
Pat Hartl <pat@pathar.tl>
2023-03-23 19:21:41 -0500

    Snippet for setting a game's compatibility mode

---
+++ LANCommander/Snippets/Examples/Set Compatibility Mode.ps1
@@ -0,0 +1,12 @@
+﻿# Available Options:
+# WIN95
+# WIN98
+# WINXPSP2
+# WINXPSP3
+# VISTARTM
+# VISTASP1
+# VISTASP2
+# WIN7RTM
+# WIN8RTM
+# See: https://ss64.com/nt/syntax-compatibility.html
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$InstallDir\<Executable>" -Value "~ WINXPSP2 HIGHDPIAWARE" -Force

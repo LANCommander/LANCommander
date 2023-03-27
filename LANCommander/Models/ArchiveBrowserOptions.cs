e2a28a57ea586e5fca3c95cf4f3853000f539a7b
Pat Hartl <pat@pathar.tl>
2023-03-27 01:17:44 -0500

    Add option to select directories in archive browsers

--- LANCommander/Models/ArchiveBrowserOptions.cs
+++ LANCommander/Models/ArchiveBrowserOptions.cs
@@ -4,6 +4,7 @@
     {
         public Guid ArchiveId { get; set; }
         public bool Select { get; set; }
-        public bool Multiple { get; set; }
+        public bool Multiple { get; set; } = false;
+        public bool AllowDirectories { get; set; } = false;
     }
 }

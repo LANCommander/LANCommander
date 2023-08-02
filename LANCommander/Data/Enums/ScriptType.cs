6df312ef2bdaddcee32d5576838e87a970cb2dd5
Pat Hartl <pat@pathar.tl>
2023-08-02 17:49:47 -0500

    Use display name for type column in scripts table

--- LANCommander/Data/Enums/ScriptType.cs
+++ LANCommander/Data/Enums/ScriptType.cs
@@ -1,12 +1,18 @@
-﻿namespace LANCommander.Data.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LANCommander.Data.Enums
 {
     public enum ScriptType
     {
         Install,
         Uninstall,
+        [Display(Name = "Name Change")]
         NameChange,
+        [Display(Name = "Key Change")]
         KeyChange,
+        [Display(Name = "Save Upload")]
         SaveUpload,
+        [Display(Name = "Save Download")]
         SaveDownload
     }
 }

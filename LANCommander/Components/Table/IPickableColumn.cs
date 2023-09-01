4f853a74444e57154e269ee074c74a199ee35d93
Pat Hartl <pat@pathar.tl>
2023-08-31 19:29:04 -0500

    WIP pickable column component without much config

---
+++ LANCommander/Components/Table/IPickableColumn.cs
@@ -0,0 +1,11 @@
+﻿using AntDesign;
+using Microsoft.AspNetCore.Components;
+
+namespace LANCommander.Components.Table
+{
+    public interface IPickableColumn : IColumn
+    {
+        public bool Visible { get; set; }
+        public EventCallback<bool> VisibleChanged { get; set; }
+    }
+}

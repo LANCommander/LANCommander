bf63813e90cb6516f45cfaac3c30928fc5656e74
Pat Hartl <pat@pathar.tl>
2023-09-11 17:56:23 -0500

    Rename input to file picker

--- LANCommander/Models/FilePickerOptions.cs
+++ LANCommander/Models/FilePickerOptions.cs
@@ -1,8 +1,9 @@
 ﻿namespace LANCommander.Models
 {
-    public class ArchiveFilePickerOptions
+    public class FilePickerOptions
     {
         public Guid ArchiveId { get; set; }
+        public string Root { get; set; }
         public bool Select { get; set; }
         public bool Multiple { get; set; } = false;
         public bool AllowDirectories { get; set; } = false;

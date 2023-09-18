8f853fab561064b7fd57c9ac8865d34e5dc33f07
Pat Hartl <pat@pathar.tl>
2023-09-18 00:58:40 -0500

    Add profile page for users to change username/email, download saves, change password.

--- LANCommander/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ LANCommander/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -24,6 +24,26 @@ namespace LANCommander.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
+        public async Task<IActionResult> OnGet(string returnUrl = null, bool force = false)
+        {
+            if (force)
+            {
+                await _signInManager.SignOutAsync();
+                _logger.LogInformation("User logged out.");
+
+                if (returnUrl != null)
+                {
+                    return LocalRedirect(returnUrl);
+                }
+                else
+                {
+                    return LocalRedirect("/Identity/Account/Login");
+                }
+            }
+
+            return Page();
+        }
+
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
---
+++ LANCommander/Controllers/SavesController.cs
@@ -0,0 +1,36 @@
+﻿using LANCommander.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LANCommander.Controllers
+{
+    [Authorize]
+    public class SavesController : Controller
+    {
+        private readonly GameSaveService GameSaveService;
+
+        public SavesController(GameSaveService gameSaveService)
+        {
+            GameSaveService = gameSaveService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Download(Guid id)
+        {
+            var save = await GameSaveService.Get(id);
+
+            if (User == null || User.Identity?.Name != save.User?.UserName)
+                return Unauthorized();
+
+            if (save == null)
+                return NotFound();
+
+            var filename = GameSaveService.GetSavePath(save);
+
+            if (!System.IO.File.Exists(filename))
+                return NotFound();
+
+            return File(new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read), "application/zip", $"{save.User?.UserName} - {save.Game?.Title} - {save.CreatedOn.ToString("MM-dd-yyyy.hh-mm")}");
+        }
+    }
+}

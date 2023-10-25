5e47d8fa3d7a418270f22c914b908c6d4aed9813
Pat Hartl <pat@pathar.tl>
2023-10-24 19:13:09 -0500

    Remove display attributes

--- LANCommander.SDK/Enums/ScriptType.cs
+++ LANCommander.SDK/Enums/ScriptType.cs
@@ -4,15 +4,10 @@
     {
         Install,
         Uninstall,
-        [Display(Name = "Name Change")]
         NameChange,
-        [Display(Name = "Key Change")]
         KeyChange,
-        [Display(Name = "Save Upload")]
         SaveUpload,
-        [Display(Name = "Save Download")]
         SaveDownload,
-        [Display(Name = "Detect Install")]
         DetectInstall
     }
 }

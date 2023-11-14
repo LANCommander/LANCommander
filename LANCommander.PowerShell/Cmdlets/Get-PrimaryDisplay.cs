baa2b9b20692ffefdfc488f9510c6387677f370e
Pat Hartl <pat@pathar.tl>
2023-11-13 23:09:06 -0600

    Started adding PowerShell cmdlets useful for LANCommander scripting

---
+++ LANCommander.PowerShell/Cmdlets/Get-PrimaryDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Management.Automation;
+using System.Windows.Forms;
+
+namespace LANCommander.PowerShell.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "PrimaryDisplay")]
+    [OutputType(typeof(string))]
+    public class GetPrimaryDisplayCmdlet : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            var screens = Screen.AllScreens;
+
+            WriteObject(screens.First(s => s.Primary));
+        }
+    }
+}
---
+++ LANCommander.PowerShell/Cmdlets/Write-GameManifest.cs
@@ -0,0 +1,24 @@
+﻿using LANCommander.SDK;
+using LANCommander.SDK.Helpers;
+using System.Management.Automation;
+
+namespace LANCommander.PowerShell.Cmdlets
+{
+    [Cmdlet(VerbsCommunications.Write, "GameManifest")]
+    [OutputType(typeof(string))]
+    public class WriteGameManifestCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public string Path { get; set; }
+
+        [Parameter(Mandatory = true, Position = 1, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public GameManifest Manifest { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var destination = ManifestHelper.Write(Manifest, Path);
+
+            WriteObject(destination);
+        }
+    }
+}

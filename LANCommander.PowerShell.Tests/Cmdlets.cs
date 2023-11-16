e723a5345be26b313b66710d22d3b0df60306742
Pat Hartl <pat@pathar.tl>
2023-11-16 15:22:20 -0600

    Scaffold tests for cmdlets

---
+++ LANCommander.PowerShell.Tests/Cmdlets.cs
@@ -0,0 +1,36 @@
+﻿using LANCommander.PowerShell.Cmdlets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace LANCommander.PowerShell.Tests
+{
+    [TestClass]
+    public class CmdletTests
+    {
+        [TestMethod]
+        public void ConvertToSerializedBase64ShouldBeDeserializable()
+        {
+            var testPhrase = "Hello world! This should be deserializable back to its original form.";
+
+            var encodingCmdlet = new ConvertToSerializedBase64Cmdlet()
+            {
+                Input = testPhrase
+            };
+
+            var encodingResults = encodingCmdlet.Invoke().OfType<string>().ToList();
+
+            Assert.AreEqual(1, encodingResults.Count);
+
+            var decodingCmdlet = new ConvertFromSerializedBase64Cmdlet()
+            {
+                Input = encodingResults.First()
+            };
+
+            var decodingResults = decodingCmdlet.Invoke().OfType<string>().ToList();
+
+            Assert.AreEqual(1, encodingResults.Count);
+            Assert.AreEqual(testPhrase, decodingResults.First());
+        }
+    }
+}

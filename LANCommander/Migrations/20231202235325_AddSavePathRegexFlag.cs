d4fe8db48d3bf975bb01c0403ee1d333170d7414
Pat Hartl <pat@pathar.tl>
2023-12-02 17:53:54 -0600

    Add regex flag to save paths

---
+++ LANCommander/Migrations/20231202235325_AddSavePathRegexFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LANCommander.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSavePathRegexFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRegex",
+                table: "SavePaths",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRegex",
+                table: "SavePaths");
+        }
+    }
+}

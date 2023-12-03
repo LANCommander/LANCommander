c48d5b5d59fd4943b904a823f188eb1e95ed11f6
Pat Hartl <pat@pathar.tl>
2023-12-03 14:29:51 -0600

    Add migration to fix PowerShell scripts that were updated to use Get-PrimaryDisplay

---
+++ LANCommander/Migrations/20231203202813_FixScriptDisplayBounds.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LANCommander.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixScriptDisplayBounds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Scripts SET Contents = REPLACE(Contents, '$Display.Width', '$Display.Bounds.Width')");
+            migrationBuilder.Sql("UPDATE Scripts SET Contents = REPLACE(Contents, '$Display.Height', '$Display.Bounds.Height')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

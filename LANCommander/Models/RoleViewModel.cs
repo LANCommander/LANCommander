3cb6e567b3508180b280b0f493b2153d809cc79c
Pat Hartl <pat@pathar.tl>
2023-12-04 17:25:43 -0600

    Add edit page for roles

--- LANCommander/Models/RoleViewModel.cs
+++ LANCommander/Models/RoleViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class RoleViewModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public int Users { get; set; }
         public int Collections { get; set; }

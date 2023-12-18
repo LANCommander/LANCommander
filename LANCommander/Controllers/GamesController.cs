382fbdd99fb02630606440c4b2241bac96049d9c
Pat Hartl <pat@pathar.tl>
2023-12-17 20:45:15 -0600

    Switch export to a ZIP archive that gets generated on the fly and includes archives and media

--- LANCommander/Controllers/GamesController.cs
+++ LANCommander/Controllers/GamesController.cs
@@ -2,8 +2,7 @@
 using LANCommander.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Reflection.Metadata;
-using System.Text;
+using System.IO.Compression;
 
 namespace LANCommander.Controllers
 {
@@ -11,19 +10,64 @@ namespace LANCommander.Controllers
     public class GamesController : BaseController
     {
         private GameService GameService;
+        private MediaService MediaService;
+        private ArchiveService ArchiveService;
 
         public GamesController(GameService gameService)
         {
             GameService = gameService;
         }
 
-        public async Task<IActionResult> Export(Guid id)
+        public async Task Export(Guid id)
         {
             var manifest = await GameService.GetManifest(id);
 
-            var serializedManifest = ManifestHelper.Serialize(manifest);
+            Response.ContentType = "application/octet-stream";
+            Response.Headers.Append("Content-Disposition", @$"attachment; filename=""{manifest.Title}.Export.zip""");
 
-            return File(Encoding.UTF8.GetBytes(serializedManifest), "application/x-yaml", $"{manifest.Title}.Export.yml");
+            using (ZipArchive export = new ZipArchive(Response.BodyWriter.AsStream(), ZipArchiveMode.Create))
+            {
+                var serializedManifest = ManifestHelper.Serialize(manifest);
+
+                using (var manistream = new MemoryStream())
+                using (var sw = new StreamWriter(manistream))
+                {
+                    sw.Write(serializedManifest);
+                    var manifestEntry = export.CreateEntry("_manifest.yml", CompressionLevel.NoCompression);
+
+                    using (var entryStream = manifestEntry.Open())
+                    {
+                        manistream.Seek(0, SeekOrigin.Begin);
+                        manistream.CopyTo(entryStream);
+                    }
+                }
+
+                if (manifest.Media != null)
+                foreach (var media in manifest.Media)
+                {
+                    var mediaFilePath = await MediaService.GetImagePath(media.Id);
+                    var entry = export.CreateEntry($"Media/{media.FileId}", CompressionLevel.NoCompression);
+
+                    using (var entryStream = entry.Open())
+                    using (var fileStream = System.IO.File.OpenRead(mediaFilePath))
+                    {
+                        await fileStream.CopyToAsync(entryStream);
+                    }
+                }
+
+                if (manifest.Archives != null)
+                foreach (var archive in manifest.Archives)
+                {
+                    var archiveFilePath = ArchiveService.GetArchiveFileLocation(archive.ObjectKey);
+                    var entry = export.CreateEntry($"Archives/{archive.ObjectKey}", CompressionLevel.NoCompression);
+
+                    using (var entryStream = entry.Open())
+                    using (var fileStream = System.IO.File.OpenRead(archiveFilePath))
+                    {
+                        await fileStream.CopyToAsync(entryStream);
+                    }
+                }
+            }
         }
 
         [HttpPost]

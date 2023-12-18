eef20c600e90402625e58feac634a5fa0580f941
Pat Hartl <pat@pathar.tl>
2023-12-18 12:13:01 -0600

    Add test for parsing exit codes from PowerShell scripts

---
+++ LANCommander.PowerShell.Tests/Runtime.cs
@@ -0,0 +1,35 @@
+﻿using LANCommander.SDK.Helpers;
+using LANCommander.SDK.PowerShell;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LANCommander.PowerShell.Tests
+{
+    [TestClass]
+    public class RuntimeTests
+    {
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(200)]
+        [DataRow(404)]
+        public void ExitCodeShouldParse(int errorCode)
+        {
+            var testScript = $"$Host.SetShouldExit({errorCode})";
+
+            var tempFile = ScriptHelper.SaveTempScript(testScript);
+
+            var script = new PowerShellScript();
+
+            script.UseFile(tempFile);
+
+            var result = script.Execute();
+
+            Assert.AreEqual(errorCode, result);
+        }
+    }
+}

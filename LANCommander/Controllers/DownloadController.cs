2c5501dc4fa6072a717d544af80b5c8ccc8035cb
Pat Hartl <pat@pathar.tl>
2023-12-27 17:23:33 -0600

    Fix save downloading from profile page

--- LANCommander/Controllers/DownloadController.cs
+++ LANCommander/Controllers/DownloadController.cs
@@ -12,6 +12,7 @@ namespace LANCommander.Controllers
     public class DownloadController : Controller
     {
         private readonly ArchiveService ArchiveService;
+        private readonly GameSaveService GameSaveService;
         private readonly LANCommanderSettings Settings = SettingService.GetSettings();
 
         public DownloadController(ArchiveService archiveService)
@@ -39,6 +40,25 @@ namespace LANCommander.Controllers
                 name = $"{archive.Redistributable.Name.SanitizeFilename()}.zip";
 
             return File(new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read), "application/octet-stream", name);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Save(Guid id)
+        {
+            var save = await GameSaveService.Get(id);
+
+            if (User == null || User.Identity?.Name != save.User?.UserName)
+                return Unauthorized();
+
+            if (save == null)
+                return NotFound();
+
+            var filename = GameSaveService.GetSavePath(save);
+
+            if (!System.IO.File.Exists(filename))
+                return NotFound();
+
+            return File(new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read), "application/zip", $"{save.User?.UserName} - {(save.Game == null ? "Unknown" : save.Game?.Title)} - {save.CreatedOn.ToString("MM-dd-yyyy.hh-mm")}.zip");
         }
     }
 }

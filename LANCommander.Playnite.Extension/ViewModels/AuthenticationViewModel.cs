9dbd6059570b0bfaad5fbe300f67fb5b4db0e9b1
Pat Hartl <pat@pathar.tl>
2024-02-18 17:24:06 -0600

    Fix missing authentication view model after rename

---
+++ LANCommander.Playnite.Extension/ViewModels/AuthenticationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LANCommander.PlaynitePlugin.ViewModels
+{
+    internal class AuthenticationViewModel
+    {
+        public string ServerAddress { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}

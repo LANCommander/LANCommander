6428c726892aa1fc3a43407c8b56148fd4ba45f3
Pat Hartl <pat@pathar.tl>
2024-05-13 01:53:41 -0500

    Remove user information from SDK game details

--- LANCommander.SDK/Models/BaseModel.cs
+++ LANCommander.SDK/Models/BaseModel.cs
@@ -6,8 +6,6 @@ namespace LANCommander.SDK.Models
     {
         public Guid Id { get; set; }
         public DateTime CreatedOn { get; set; }
-        public virtual User CreatedBy { get; set; }
         public DateTime UpdatedOn { get; set; }
-        public virtual User UpdatedBy { get; set; }
     }
 }

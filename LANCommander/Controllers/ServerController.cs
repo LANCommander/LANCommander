b8a6b07d4b917479b0b5c227727d8c1ced1f6065
Pat Hartl <pat@pathar.tl>
2024-07-03 23:48:57 -0500

    Allow exporting of servers

--- LANCommander/Controllers/ServerController.cs
+++ LANCommander/Controllers/ServerController.cs
@@ -1,15 +1,91 @@
-﻿using LANCommander.Services;
+﻿using AutoMapper;
+using LANCommander.SDK.Helpers;
+using LANCommander.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.IO.Compression;
 
 namespace LANCommander.Controllers
 {
     public class ServerController : Controller
     {
         private readonly ServerService ServerService;
+        private readonly IMapper Mapper;
 
-        public ServerController(ServerService serverService)
+        public ServerController(ServerService serverService, IMapper mapper)
         {
-            this.ServerService = serverService;
+            ServerService = serverService;
+            Mapper = mapper;
+        }
+
+        [HttpGet("/Server/{id:guid}/Export/Full")]
+        public async Task ExportFull(Guid id)
+        {
+            var server = await ServerService.Get(id);
+
+            if (server == null)
+            {
+                Response.StatusCode = StatusCodes.Status404NotFound;
+                return;
+            }
+
+            Response.ContentType = "application/octet-stream";
+            Response.Headers.Append("Content-Disposition", @$"attachment; filename=""{server.Name}.lcx""");
+
+            using (ZipArchive export = new ZipArchive(Response.BodyWriter.AsStream(), ZipArchiveMode.Create))
+            {
+                var manifest = Mapper.Map<SDK.Models.Server>(server);
+                var serializedManifest = ManifestHelper.Serialize(manifest);
+
+                using (var manistream = new MemoryStream())
+                using (var sw = new StreamWriter(manistream))
+                {
+                    sw.Write(serializedManifest);
+                    sw.Flush();
+
+                    var manifestEntry = export.CreateEntry(ManifestHelper.ManifestFilename, CompressionLevel.NoCompression);
+
+                    using (var entryStream = manifestEntry.Open())
+                    {
+                        manistream.Seek(0, SeekOrigin.Begin);
+                        manistream.CopyTo(entryStream);
+                    }
+                }
+
+                if (server.Scripts != null)
+                foreach (var script in server.Scripts)
+                {
+                    using (var scriptStream = new MemoryStream())
+                    using (var sw = new StreamWriter(scriptStream))
+                    {
+                        sw.Write(script.Contents);
+                        sw.Flush();
+
+                        var scriptEntry = export.CreateEntry($"Scripts/{script.Id}", CompressionLevel.NoCompression);
+
+                        using (var entryStream = scriptEntry.Open())
+                        {
+                            scriptStream.Seek(0, SeekOrigin.Begin);
+                            scriptStream.CopyTo(entryStream);
+                        }
+                    }
+                }
+
+                var files = Directory.EnumerateFileSystemEntries($"{server.WorkingDirectory}", "*", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    var entryName = file.Substring(server.WorkingDirectory.Length, file.Length - server.WorkingDirectory.Length).Replace(Path.DirectorySeparatorChar, '/');
+
+                    if (System.IO.File.Exists(file))
+                    {
+                        export.CreateEntryFromFile(file, $"Files/{entryName}", CompressionLevel.NoCompression);
+                    }
+                    else if (System.IO.Directory.Exists(file))
+                    {
+                        export.CreateEntry($"Files/{entryName}/");
+                    }
+                }
+            }
         }
 
         [HttpGet("/Server/{id:guid}/{*path}")]

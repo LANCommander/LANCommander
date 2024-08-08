a27a999f842ba2611e726def7bc906401fb6670d
Pat Hartl <pat@pathar.tl>
2024-08-07 19:41:42 -0500

    Add CLI project

---
+++ LANCommander.Launcher.CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using LANCommander.Launcher.Services.Extensions;
+using LANCommander.Launcher.Services;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddLANCommander();
+
+using IHost host = builder.Build();
+
+using var scope = host.Services.CreateScope();
+
+var commandLineService = scope.ServiceProvider.GetService<CommandLineService>();
+
+await commandLineService.ParseCommandLineAsync(args);

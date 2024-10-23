e2568755ead77fbc8ac19a1ef814441872a0bc34
Pat Hartl <pat@pathar.tl>
2024-10-23 18:31:24 -0500

    Don't try to grab profile if token doesn't validate

--- LANCommander.Launcher.Services/ProfileService.cs
+++ LANCommander.Launcher.Services/ProfileService.cs
@@ -46,45 +46,48 @@ namespace LANCommander.Launcher.Services
             Settings.Authentication.RefreshToken = token.RefreshToken;
 
             Client.UseToken(token);
-            await Client.ValidateTokenAsync(token);
 
-            SettingService.SaveSettings(Settings);
-
-            var remoteProfile = await Client.Profile.GetAsync();
-
-            Settings.Profile.Id = remoteProfile.Id;
-            Settings.Profile.Alias = String.IsNullOrWhiteSpace(remoteProfile.Alias) ? remoteProfile.UserName : remoteProfile.Alias;
-
-            try
+            if (await Client.ValidateTokenAsync())
             {
-                var tempAvatarPath = await Client.Profile.DownloadAvatar();
 
-                if (!String.IsNullOrWhiteSpace(tempAvatarPath))
+                SettingService.SaveSettings(Settings);
+
+                var remoteProfile = await Client.Profile.GetAsync();
+
+                Settings.Profile.Id = remoteProfile.Id;
+                Settings.Profile.Alias = String.IsNullOrWhiteSpace(remoteProfile.Alias) ? remoteProfile.UserName : remoteProfile.Alias;
+
+                try
                 {
-                    var media = new Media
+                    var tempAvatarPath = await Client.Profile.DownloadAvatar();
+
+                    if (!String.IsNullOrWhiteSpace(tempAvatarPath))
                     {
-                        FileId = Guid.NewGuid(),
-                        Type = SDK.Enums.MediaType.Avatar,
-                        MimeType = MediaTypeNames.Image.Png,
-                        Crc32 = SDK.Services.MediaService.CalculateChecksum(tempAvatarPath),
-                    };
+                        var media = new Media
+                        {
+                            FileId = Guid.NewGuid(),
+                            Type = SDK.Enums.MediaType.Avatar,
+                            MimeType = MediaTypeNames.Image.Png,
+                            Crc32 = SDK.Services.MediaService.CalculateChecksum(tempAvatarPath),
+                        };
 
-                    media = await MediaService.Add(media);
+                        media = await MediaService.Add(media);
 
-                    var localPath = MediaService.GetImagePath(media);
+                        var localPath = MediaService.GetImagePath(media);
 
-                    if (File.Exists(tempAvatarPath))
-                        File.Move(tempAvatarPath, localPath);
+                        if (File.Exists(tempAvatarPath))
+                            File.Move(tempAvatarPath, localPath);
 
-                    Settings.Profile.AvatarId = media.Id;
+                        Settings.Profile.AvatarId = media.Id;
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                Logger?.LogError(ex, "Could not download avatar");
-            }
+                catch (Exception ex)
+                {
+                    Logger?.LogError(ex, "Could not download avatar");
+                }
 
-            SettingService.SaveSettings(Settings);
+                SettingService.SaveSettings(Settings);
+            }
         }
 
         public async Task Login(string serverAddress, string username, string password)

37ba817706d8249cd139c4259297077e77a73b0d
Pat Hartl <pat@pathar.tl>
2024-12-15 00:51:53 -0600

    Include relevant data in depot, only list accessible games

--- LANCommander.Server/Controllers/Api/DepotController.cs
+++ LANCommander.Server/Controllers/Api/DepotController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using LANCommander.SDK.Models;
 using LANCommander.Server.Data;
 using LANCommander.Server.Data.Models;
 using LANCommander.Server.Services;
@@ -43,30 +44,73 @@ namespace LANCommander.Server.Controllers.Api
             var user = await UserService.GetAsync(User?.Identity?.Name);
             var library = await LibraryService.GetByUserIdAsync(user.Id);
 
-            library = await LibraryService.Include(l => l.Games).GetAsync(library.Id);
-
             var libraryGameIds = library.Games.Where(g => g != null).Select(g => g.Id).ToList();
 
             var games = await Cache.GetOrSetAsync("DepotGames", async _ =>
             {
                 return await GameService
                     .Include(g => g.Media.Where(m => m.Type == SDK.Enums.MediaType.Cover))
+                    .Include(g => g.Collections.Select(c => c.Id))
+                    .Include(g => g.Developers.Select(c => c.Id))
+                    .Include(g => g.Genres.Select(g => g.Id))
+                    .Include(g => g.MultiplayerModes)
+                    .Include(g => g.Platforms.Select(p => p.Id))
+                    .Include(g => g.Publishers.Select(c => c.Id))
+                    .Include(g => g.Tags.Select(t => t.Id))
                     .GetAsync<SDK.Models.DepotGame>();
             }, TimeSpan.MaxValue);
 
-            foreach (var game in games)
+            var accessibleGames = games.ToList();
+
+            if (Settings.Roles.RestrictGamesByCollection && !User.IsInRole(RoleService.AdministratorRoleName))
+            {
+                var roles = await UserService.GetRolesAsync(User?.Identity.Name);
+
+                var accessibleCollectionIds = roles.SelectMany(r => r.Collections.Select(c => c.Id)).Distinct();
+
+                accessibleGames = games.Where(g => g.Collections.Any(c => accessibleCollectionIds.Contains(c.Id))).ToList();
+
+                foreach (var game in accessibleGames)
+                {
+                    game.Collections = game.Collections.Where(c => accessibleCollectionIds.Contains(c.Id));
+                }
+            }
+
+            foreach (var game in accessibleGames)
             {
                 if (libraryGameIds.Contains(game.Id))
                     game.InLibrary = true;
             }
 
-            return games;
+            return accessibleGames;
         }
 
         [HttpGet("Games/{id}")]
         public async Task<SDK.Models.DepotGame> GetGameAsync(Guid id)
         {
-            var game = await GameService.GetAsync(id);
+            var game = await Cache.GetOrSetAsync($"DepotGames/{id}", async _ =>
+            {
+                return await GameService
+                    .Include(g => g.Actions)
+                    .Include(g => g.Archives)
+                    .Include(g => g.BaseGame)
+                    .Include(g => g.Categories)
+                    .Include(g => g.Collections)
+                    .Include(g => g.DependentGames)
+                    .Include(g => g.Developers)
+                    .Include(g => g.Engine)
+                    .Include(g => g.Genres)
+                    .Include(g => g.Media)
+                    .Include(g => g.MultiplayerModes)
+                    .Include(g => g.Platforms)
+                    .Include(g => g.Publishers)
+                    .Include(g => g.Redistributables)
+                    .Include(g => g.SavePaths)
+                    .Include(g => g.Scripts)
+                    .Include(g => g.Tags)
+                    .GetAsync(id);
+            });
+
             var user = await UserService.GetAsync(User?.Identity?.Name);
             var library = await LibraryService.GetByUserIdAsync(user.Id);
 

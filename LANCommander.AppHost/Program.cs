529f628ccf6c3e3d72590f67e88169846e25a4d5
Aaron Powell <me@aaron-powell.com>
2025-01-03 16:29:57 +1100

    Added support for MySQL via Aspire

--- LANCommander.AppHost/Program.cs
+++ LANCommander.AppHost/Program.cs
@@ -1,23 +1,45 @@
-using Aspire.Hosting;
 using Microsoft.Extensions.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var usePostgres = builder.Configuration.GetValue("UsePostgres", false);
+var dbType = builder.Configuration.GetValue("DatabaseType", "SQLite");
 
 var server = builder.AddProject<Projects.LANCommander_Server>("lancommander-server");
 
-if (usePostgres)
+switch (dbType)
 {
-    var postgres = builder.AddPostgres("pg")
-    .WithDataVolume()
-    .WithPgAdmin()
-    .AddDatabase("lancommander");
+    case "PostgreSQL":
+        ConfigurePostgres(builder, server);
+        break;
 
-    server
-    .WithReference(postgres)
-    .WaitFor(postgres)
-    .WithEnvironment("DatabaseProvider", "PostgreSQL");
+    case "MySQL":
+        ConfigureMySQL(builder, server);
+        break;
 }
 
 builder.Build().Run();
+
+static void ConfigurePostgres(IDistributedApplicationBuilder builder, IResourceBuilder<ProjectResource> server)
+{
+    var postgres = builder.AddPostgres("pg")
+                .WithDataVolume()
+                .WithPgAdmin()
+                .AddDatabase("lancommander");
+
+    server
+        .WithReference(postgres)
+        .WaitFor(postgres)
+        .WithEnvironment("DatabaseProvider", "PostgreSQL");
+}
+
+static void ConfigureMySQL(IDistributedApplicationBuilder builder, IResourceBuilder<ProjectResource> server)
+{
+    var mysql = builder.AddMySql("mysql")
+                .WithDataVolume()
+                .WithPhpMyAdmin()
+                .AddDatabase("lancommander");
+    server
+        .WithReference(mysql)
+        .WaitFor(mysql)
+        .WithEnvironment("DatabaseProvider", "MySQL");
+}

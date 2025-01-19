46b60dfa9bb9146faae57269588ca6e0b73763e0
Pat Hartl <pat@pathar.tl>
2025-01-19 13:57:29 -0600

    Include user ID in JWT

--- LANCommander.Server.Services/AuthenticationService.cs
+++ LANCommander.Server.Services/AuthenticationService.cs
@@ -56,6 +56,7 @@ namespace LANCommander.Server.Services
             var authClaims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             };
 

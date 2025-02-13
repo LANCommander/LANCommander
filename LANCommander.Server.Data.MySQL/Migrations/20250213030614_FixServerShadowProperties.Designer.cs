b546f03a261b77be53bdf1a6759081c9b8f4c123
Pat Hartl <pat@pathar.tl>
2025-02-12 21:07:21 -0600

    Fix server shadow properties, adjust some string column lengths

--- LANCommander.Server.Data.MySQL/Migrations/20250213030614_FixServerShadowProperties.Designer.cs
+++ LANCommander.Server.Data.MySQL/Migrations/20250213030614_FixServerShadowProperties.Designer.cs
@@ -4,6 +4,7 @@ using LANCommander.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LANCommander.Server.Data.MySQL.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250213030614_FixServerShadowProperties")]
+    partial class FixServerShadowProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LANCommander.Server.Data.MySQL/Migrations/20250213030614_FixServerShadowProperties.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LANCommander.Server.Data.MySQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixServerShadowProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServerConsoles_Servers_ServerId1",
+                table: "ServerConsoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServerHttpPath_Servers_ServerId1",
+                table: "ServerHttpPath");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServerHttpPath_ServerId1",
+                table: "ServerHttpPath");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServerConsoles_ServerId1",
+                table: "ServerConsoles");
+
+            migrationBuilder.DropColumn(
+                name: "ServerId1",
+                table: "ServerHttpPath");
+
+            migrationBuilder.DropColumn(
+                name: "ServerId1",
+                table: "ServerConsoles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "ServerHttpPath",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LocalPath",
+                table: "ServerHttpPath",
+                type: "varchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "ServerConsoles",
+                type: "varchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "ServerConsoles",
+                type: "varchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ServerConsoles",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Host",
+                table: "ServerConsoles",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "ServerHttpPath",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LocalPath",
+                table: "ServerHttpPath",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1024)",
+                oldMaxLength: 1024)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ServerId1",
+                table: "ServerHttpPath",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "ServerConsoles",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1024)",
+                oldMaxLength: 1024)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "ServerConsoles",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(128)",
+                oldMaxLength: 128)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ServerConsoles",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Host",
+                table: "ServerConsoles",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ServerId1",
+                table: "ServerConsoles",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServerHttpPath_ServerId1",
+                table: "ServerHttpPath",
+                column: "ServerId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServerConsoles_ServerId1",
+                table: "ServerConsoles",
+                column: "ServerId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServerConsoles_Servers_ServerId1",
+                table: "ServerConsoles",
+                column: "ServerId1",
+                principalTable: "Servers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServerHttpPath_Servers_ServerId1",
+                table: "ServerHttpPath",
+                column: "ServerId1",
+                principalTable: "Servers",
+                principalColumn: "Id");
+        }
+    }
+}

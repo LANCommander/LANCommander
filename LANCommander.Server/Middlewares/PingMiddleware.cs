086a17e1c82a45e2b03ac663fb6c019eb0e03fac
Pat Hartl <pat@pathar.tl>
2025-03-11 02:05:31 -0500

    Actually fix ping blocking requests

--- LANCommander.Server/Middlewares/PingMiddleware.cs
+++ LANCommander.Server/Middlewares/PingMiddleware.cs
@@ -10,9 +10,10 @@ namespace LANCommander.Server
             try
             {
                 if (context.Request.Method == "HEAD" && context.Request.Headers.ContainsKey("X-Ping"))
+                {
                     context.Response.Headers["X-Pong"] = context.Request.Headers["X-Ping"].First().FastReverse();
-
-                return;
+                    return;
+                }
             }
             catch { }
             

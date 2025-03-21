0e6c76673425e5b4e3d85a4d7f680aefe0d0a5d7
Pat Hartl <pat@pathar.tl>
2025-03-21 18:22:21 -0500

    Fix matrix

--- LANCommander.Build/Nightly.ps1
+++ LANCommander.Build/Nightly.ps1
@@ -6,12 +6,12 @@ $Version = Get-LastSemVerTag
 $Version = [semver]"$($Version.Major).$($Version.Minor).$($Version.Patch)-nightly.$(Get-Date -Format "yyyyMMddHHmm")"
 
 $BuildTargets = 
-    @{ Platform = 'Windows'; $Architecture = 'x64';    $Runtime = 'win' },
-    @{ Platform = 'Windows'; $Architecture = 'arm64';  $Runtime = 'win' },
-    @{ Platform = 'Linux';   $Architecture = 'x64';    $Runtime = 'linux' },
-    @{ Platform = 'Windows'; $Architecture = 'arm64';  $Runtime = 'linux' },
-    @{ Platform = 'Windows'; $Architecture = 'x64';    $Runtime = 'osx' },
-    @{ Platform = 'Windows'; $Architecture = 'arm64';  $Runtime = 'osx' }
+    @{ Platform = 'Windows'; Architecture = 'x64';   Runtime = 'win' },
+    @{ Platform = 'Windows'; Architecture = 'arm64'; Runtime = 'win' },
+    @{ Platform = 'Linux';   Architecture = 'x64';   Runtime = 'linux' },
+    @{ Platform = 'Windows'; Architecture = 'arm64'; Runtime = 'linux' },
+    @{ Platform = 'Windows'; Architecture = 'x64';   Runtime = 'osx' },
+    @{ Platform = 'Windows'; Architecture = 'arm64'; Runtime = 'osx' }
 
 Write-Host "Building Targets"
 

006af311ad3f1e6c9bd998a07b66b1c2d50a39f9
RattleSN4K3 <RattleSN4K3@gmail.com>
2025-04-22 17:50:46 +0200

    Scrape PCGamingWiki via its API content through cargoquery, instead of possible spambot-protected page
    In cases the page is not queried properly (likely due to wrong UserAgent) and will have a dynamic HTMl page which will forward to the correct page. Instead of having that mechanism in place, the API endpoint is queried to get the HTML content by the cargoquery API.

--- LANCommander.Server/PCGamingWikiClient.cs
+++ LANCommander.Server/PCGamingWikiClient.cs
@@ -1,5 +1,6 @@
 ﻿using HtmlAgilityPack;
 using LANCommander.SDK;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -13,6 +14,45 @@ namespace LANCommander.PCGamingWiki
 {
     public class PCGamingWikiClient
     {
+        #region Internal models
+
+        public class GetPageIdModel
+        {
+            public class Cargoquery
+            {
+                public class Title
+                {
+                    public string PageID { get; set; }
+                    public string Page { get; set; }
+
+                    [JsonProperty("Steam AppID")]
+                    public string SteamAppID { get; set; }
+                }
+                public Title title { get; set; }
+            }
+
+            public List<Cargoquery> cargoquery { get; } = new List<Cargoquery>();
+        }
+
+        public class GetPageContentModel
+        {
+            public class Parse
+            {
+                public class Text
+                {
+                    [JsonProperty("*")]
+                    public string content { get; set; }
+                }
+
+                public string title { get; set; }
+                public int pageid { get; set; }
+                public Text text { get; set; }
+            }
+            public Parse parse { get; set; }
+        }
+        
+        #endregion
+
         private HttpClient Client;
 
         public PCGamingWikiClient()
@@ -41,25 +81,38 @@ namespace LANCommander.PCGamingWiki
 
         public async Task<Dictionary<string, int>> GetMultiplayerPlayerCounts(string keyword)
         {
-            var url = await Search(keyword);
+            var pageId = await GetPageIDAsync(keyword);
+            if (pageId != null)
+            {
+                var pageContent = await GetPageContentAsync(pageId);
 
-            return await GetMultiplayerPlayerCounts(url);
+                var dom = new HtmlDocument();
+                dom.LoadHtml(pageContent ?? string.Empty);
+                return GetMultiplayerPlayerCounts(dom);
+            }
+
+            return [];
         }
 
-        public async Task<Dictionary<string, int>> GetMultiplayerPlayerCounts(Uri url)
+        public Dictionary<string, int> GetMultiplayerPlayerCounts(Uri url)
         {
+            if (url == null)
+                return [];
+
+            HtmlWeb web = new();
+            HtmlDocument dom = web.Load(url);
+            return GetMultiplayerPlayerCounts(dom);
+        }
+
+        protected Dictionary<string, int> GetMultiplayerPlayerCounts(HtmlDocument dom)
+        {
+            ArgumentNullException.ThrowIfNull(dom);
+
             var results = new Dictionary<string, int>();
 
-            if (url == null)
-                return results;
-
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument dom = web.Load(url);
-
             HtmlNode multiplayerTable = dom.GetElementbyId("table-network-multiplayer");
-
             if (multiplayerTable == null)
-                return null;
+                return results;
 
             var multiplayerRows = multiplayerTable.SelectNodes(".//tr[contains(@class, 'table-network-multiplayer-body-row')]");
             var multiplayerAbbrs = multiplayerTable.SelectNodes(".//abbr");
@@ -98,6 +151,53 @@ namespace LANCommander.PCGamingWiki
             }
 
             return results;
+        }
+
+        protected async Task<string?> GetPageIDAsync(string keyword)
+        {
+            try
+            {
+                string getPageIdUrl = $"https://www.pcgamingwiki.com/w/api.php?action=cargoquery&format=json&tables=Infobox_game&fields=Infobox_game._pageID%3DPageID%2C_pageName%3DPage%2CSteam_AppID&where=Infobox_game._pageName%3D%22{HttpUtility.UrlEncode(keyword)}%22&formatversion=1";
+
+                // HttpClient.GetFromJsonAsync<> does not seem to deserialize list properly (at least with the usedmodel)
+                // read json and parse via Newtonsoft lib
+                string getPageIdJson = await Client.GetStringAsync(getPageIdUrl);
+                var pageId = JsonConvert.DeserializeObject<GetPageIdModel>(getPageIdJson);
+
+                if (pageId == null || pageId.cargoquery == null || pageId.cargoquery.Count != 1)
+                    return null;
+
+                var title = pageId.cargoquery[0]?.title;
+                return title?.PageID;
+            }
+            catch (Exception ex)
+            {
+                // TODO: add logging
+                //Logger?.LogError(ex, $"Could not parse page id from PCGamingWiki for retrieving page with keyword {keyword}");
+            }
+
+            return null;
+        }
+
+        protected async Task<string?> GetPageContentAsync(string pageId)
+        {
+            try
+            {
+                string getPageContentUrl = $"https://www.pcgamingwiki.com/w/api.php?action=parse&format=json&prop=text&pageid={HttpUtility.UrlEncode(pageId)}";
+
+                // HttpClient.GetFromJsonAsync<> does not seem to deserialize list properly (at least with the usedmodel)
+                // read json and parse via Newtonsoft lib
+                string getPageContentJson = await Client.GetStringAsync(getPageContentUrl);
+                var pageContent = JsonConvert.DeserializeObject<GetPageContentModel>(getPageContentJson);
+                return pageContent?.parse?.text?.content;
+            }
+            catch (Exception ex)
+            {
+                // TODO: add logging
+                //Logger?.LogError(ex, $"Could not parse page content from PCGamingWiki for retrieving page with id {pageId}");
+            }
+
+            return null;
         }
     }
 }

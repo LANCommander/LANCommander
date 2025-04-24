041a0069eee77d11e5afc389fa819bff89208383
Pat Hartl <pat@pathar.tl>
2025-04-23 21:23:33 -0500

    Add DiscoveryBeacon, DiscoveryProbe, new beacon message
    Adds a discovery beacon and probe to the client and removes BeaconService from the server.

---
+++ LANCommander.Server/Startup/Beacon.cs
@@ -0,0 +1,16 @@
+using LANCommander.SDK;
+using LANCommander.Server.Services;
+
+namespace LANCommander.Server.Startup;
+
+public static class Beacon
+{
+    public static async Task StartBeaconAsync(this WebApplication app)
+    {
+        var settings = SettingService.GetSettings();
+        var client = app.Services.GetRequiredService<Client>();
+
+        if (settings.Beacon.Enabled)
+            await client.Beacon.StartBeaconAsync(settings.Beacon.Port, settings.Beacon.Address, settings.Beacon.Name);
+    }
+}

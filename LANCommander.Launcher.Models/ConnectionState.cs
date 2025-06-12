7822a926126fe13b9d0351a17ca66157d68ced22
RattleSN4K3 <RattleSN4K3@gmail.com>
2025-06-12 04:04:04 +0200

    Handle auth state in auth form, forward to specific state based on connection state

--- LANCommander.Launcher.Models/ConnectionState.cs
+++ LANCommander.Launcher.Models/ConnectionState.cs
@@ -5,4 +5,5 @@ public class ConnectionState
     public bool ValidCredentials { get; set; }
     public bool IsConnected { get; set; }
     public bool OfflineModeEnabled { get; set; }
+    public bool IsStartup { get; set; }
 }

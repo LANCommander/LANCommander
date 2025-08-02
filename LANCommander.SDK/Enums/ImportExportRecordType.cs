272195f03236f4c2b76a4c6b288a35c66843097b
Pat Hartl <pat@pathar.tl>
2025-08-01 19:15:32 -0500

    Clean up export dialog

--- LANCommander.SDK/Enums/ImportExportRecordType.cs
+++ LANCommander.SDK/Enums/ImportExportRecordType.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LANCommander.SDK.Enums;
 
 public enum ImportExportRecordType
@@ -6,23 +8,42 @@ public enum ImportExportRecordType
     Redistributable,
     Server,
     
+    [Display(Name = "Actions")]
     Action,
+    [Display(Name = "Archives")]
     Archive,
+    [Display(Name = "Collections")]
     Collection,
+    [Display(Name = "Custom Fields")]
     CustomField,
+    [Display(Name = "Developers")]
     Developer,
+    [Display(Name = "Publishers")]
     Publisher,
+    [Display(Name = "Engines")]
     Engine,
+    [Display(Name = "Genres")]
     Genre,
+    [Display(Name = "Keys")]
     Key,
+    [Display(Name = "Media")]
     Media,
+    [Display(Name = "Multiplayer Modes")]
     MultiplayerMode,
+    [Display(Name = "Platforms")]
     Platform,
+    [Display(Name = "Play Sessions")]
     PlaySession,
+    [Display(Name = "Saves")]
     Save,
+    [Display(Name = "Save Paths")]
     SavePath,
+    [Display(Name = "Scripts")]
     Script,
+    [Display(Name = "Consoles")]
     ServerConsole,
+    [Display(Name = "HTTP Paths")]
     ServerHttpPath,
+    [Display(Name = "Tag")]
     Tag,
 }

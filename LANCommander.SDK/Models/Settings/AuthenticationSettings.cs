d21a7dfc0d02b9ace3118a831716ca7f315a0b8e
Pat Hartl <pat@pathar.tl>
2025-09-29 19:51:16 -0500

    Store server address as string in Settings.yml

--- LANCommander.SDK/Models/Settings/AuthenticationSettings.cs
+++ LANCommander.SDK/Models/Settings/AuthenticationSettings.cs
@@ -1,9 +1,11 @@
 using System;
+using YamlDotNet.Serialization;
 
 namespace LANCommander.SDK.Models;
 
 public class AuthenticationSettings
 {
+    [YamlMember(typeof(string))]
     public Uri ServerAddress { get; set; }
     public string AccessToken { get; set; }
     public string RefreshToken { get; set; }

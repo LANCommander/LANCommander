f345b492634807606ceccc8f3639afb5ffbdb55f
Pat Hartl <pat@pathar.tl>
2025-12-16 00:20:48 -0600

    Apply SemVer sort ordering

--- LANCommander.SDK/Services/MigrationService.cs
+++ LANCommander.SDK/Services/MigrationService.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using LANCommander.SDK.Migrations;
 using Microsoft.Extensions.DependencyInjection;
+using Semver;
 
 namespace LANCommander.SDK.Services;
 
@@ -12,7 +13,7 @@ public class MigrationService(IServiceProvider serviceProvider)
     {
         var migrations = serviceProvider.GetServices<IMigration>();
 
-        foreach (var migration in migrations.OrderBy(m => m.Version))
+        foreach (var migration in migrations.OrderBy(m => m.Version, SemVersion.SortOrderComparer))
             await migration.ExecuteAsync();
     }
 }

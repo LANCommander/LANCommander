6021eca118dd71dd5926c3e9d2245d83bc5fb089
Pat Hartl <pat@pathar.tl>
2026-01-03 15:51:47 -0600

    Refactor time provider as JS module

---
+++ LANCommander.UI/Components/LocalTime/TimeProviderFactory.ts
@@ -0,0 +1,5 @@
+import { TimeProvider } from "./TimeProvider";
+
+export function CreateTimeProvider(): TimeProvider {
+    return new TimeProvider();
+}
--- LANCommander.UI/Main.ts
+++ LANCommander.UI/Main.ts
@@ -1,11 +1,10 @@
 import { SplitPane } from "./Components/SplitPane/SplitPane"
-import { TimeProvider } from "./Components/LocalTime/TimeProvider";
 export { CreateUploader } from "./Components/ChunkUploader/UploaderFactory";
+export { CreateTimeProvider } from "./Components/LocalTime/TimeProviderFactory";
 import { Readline } from "xterm-readline"
 import { FitAddon } from "@xterm/addon-fit"
 
 (<any>window).SplitPane = new SplitPane();
-(<any>window).TimeProvider = new TimeProvider();
 (<any>window).XtermAddons = {
     Fit: FitAddon,
     Readline: Readline

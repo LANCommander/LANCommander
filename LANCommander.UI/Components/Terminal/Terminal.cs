fbcb02a86395b46b53a34e06f61cbb347df828f5
Pat Hartl <pat@pathar.tl>
2026-01-03 23:58:50 -0600

    Move useful terminal methods to component, remove old addon registration

--- LANCommander.UI/Components/Terminal/Terminal.cs
+++ LANCommander.UI/Components/Terminal/Terminal.cs
@@ -2,6 +2,7 @@ using LANCommander.UI.Extensions;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using XtermBlazor;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace LANCommander.UI.Components;
 
@@ -16,4 +17,47 @@ public partial class Terminal : Xterm
         
         await base.OnInitializedAsync();
     }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await JS.ExecuteVoidAsync("RegisterXtermAddons");
+        
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+            await FitAsync();
+    }
+
+    /// <summary>
+    /// Write a line to the terminal with a specified log level
+    /// </summary>
+    /// <param name="message">Message to send</param>
+    /// <param name="level">LogLevel to write</param>
+    public async Task WriteLine(string message, LogLevel level = LogLevel.Information)
+    {
+        var code = level switch
+        {
+            LogLevel.Error => 31,
+            LogLevel.Warning => 33,
+            LogLevel.Debug => 37,
+            LogLevel.Trace => 36,
+            LogLevel.Information => 37,
+            _ => 37,
+        };
+        
+        await base.WriteLine($"\x1b[0;{code}m{message}");
+    }
+
+    /// <summary>
+    /// Rerenders the terminal and changes the size to fit within the element's rect. Utilizes the `addon-fit` addon.
+    /// </summary>
+    public async Task FitAsync()
+        => await Addon("addon-fit").InvokeVoidAsync("fit");
+    
+    /// <summary>
+    /// Wait for user input in the terminal
+    /// </summary>
+    /// <param name="prefix"></param>
+    public async Task<string> ReadLineAsync(string prefix = "> ")
+        => await Addon("readline").InvokeAsync<string>("read", prefix);
 }

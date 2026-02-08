1b21c77fe54247deea19d28a650af165e1760363
Pat Hartl <pat@pathar.tl>
2026-02-07 19:19:33 -0600

    Make ArchiveId nullable for file pickers

--- LANCommander.Server/Models/FilePickerOptions.cs
+++ LANCommander.Server/Models/FilePickerOptions.cs
@@ -4,7 +4,7 @@ namespace LANCommander.Server.Models
 {
     public class FilePickerOptions
     {
-        public Guid ArchiveId { get; set; }
+        public Guid? ArchiveId { get; set; }
         public string Root { get; set; }
         public bool Select { get; set; }
         public bool Multiple { get; set; } = false;

3442900dc4bd5ddaaa3463f9ffdec899223dee47
Pat Hartl <pat@pathar.tl>
2026-02-12 18:58:40 -0600

    Add migration for tools

--- LANCommander.Launcher.Data/Migrations/20260212030932_AddTools.Designer.cs
+++ LANCommander.Launcher.Data/Migrations/20260212030932_AddTools.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LANCommander.Launcher.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LANCommander.Launcher.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20260212030932_AddTools")]
+    partial class AddTools
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");

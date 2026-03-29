38eb9368eeb1dde69cdce9c00ccf47af8a0c8bf6
Pat Hartl <pat@pathar.tl>
2026-03-29 00:53:06 -0500

    Fix cover box shadow, moved cover button to separate component

---
+++ LANCommander.Launcher.Avalonia/Views/Components/CoverButton.axaml.cs
@@ -0,0 +1,44 @@
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace LANCommander.Launcher.Avalonia.Views.Components;
+
+public partial class CoverButton : UserControl
+{
+    public static readonly StyledProperty<ICommand?> CommandProperty =
+        AvaloniaProperty.Register<CoverButton, ICommand?>(nameof(Command));
+
+    public static readonly StyledProperty<object?> CommandParameterProperty =
+        AvaloniaProperty.Register<CoverButton, object?>(nameof(CommandParameter));
+
+    public ICommand? Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    public CoverButton()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnPointerEntered(PointerEventArgs e)
+    {
+        base.OnPointerEntered(e);
+        ZIndex = 100;
+    }
+
+    protected override void OnPointerExited(PointerEventArgs e)
+    {
+        base.OnPointerExited(e);
+        ZIndex = 0;
+    }
+}

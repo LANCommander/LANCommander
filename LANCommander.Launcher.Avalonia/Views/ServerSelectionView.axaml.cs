42cdf5fef874d71a207212e1f947ba6cde64aaa9
Pat Hartl <pat@pathar.tl>
2026-04-01 01:09:30 -0500

    Adjust styling of inputs, login/server select views

--- LANCommander.Launcher.Avalonia/Views/ServerSelectionView.axaml.cs
+++ LANCommander.Launcher.Avalonia/Views/ServerSelectionView.axaml.cs
@@ -1,10 +1,23 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace LANCommander.Launcher.Avalonia.Views;
 
 public partial class ServerSelectionView : UserControl
 {
+    private static readonly string[] Backgrounds =
+    {
+        "avares://LANCommander.Launcher.Avalonia/Assets/backgrounds/aoe2.jpg",
+        "avares://LANCommander.Launcher.Avalonia/Assets/backgrounds/ns2.jpg",
+        "avares://LANCommander.Launcher.Avalonia/Assets/backgrounds/css.jpg",
+        "avares://LANCommander.Launcher.Avalonia/Assets/backgrounds/bfme2.jpg",
+        "avares://LANCommander.Launcher.Avalonia/Assets/backgrounds/soldat2.jpg",
+        "avares://LANCommander.Launcher.Avalonia/Assets/backgrounds/ut2004.jpg",
+    };
+    
     public ServerSelectionView()
     {
         InitializeComponent();
@@ -13,6 +26,13 @@ public partial class ServerSelectionView : UserControl
 
     private void OnLoaded(object? sender, RoutedEventArgs e)
     {
+        try
+        {
+            var uri = new Uri(Backgrounds[Random.Shared.Next(Backgrounds.Length)]);
+            BackgroundImage.Source = new Bitmap(AssetLoader.Open(uri));
+        }
+        catch { /* silently ignore missing assets */ }
+        
         ServerAddressTextBox.Focus();
     }
 }
